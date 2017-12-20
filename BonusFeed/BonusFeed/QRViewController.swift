baf7d416172f73f509db1f91c8ad07feee589868
Ivan <romvano@gmail.com>
2017-12-20 12:31:50 +0300

    sth

--- BonusFeed/BonusFeed/QRViewController.swift
+++ BonusFeed/BonusFeed/QRViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  QRViewController.swift
 //  BonusFeed
 //
 //  Created by Daria Firsova on 18.12.2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class QRViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
