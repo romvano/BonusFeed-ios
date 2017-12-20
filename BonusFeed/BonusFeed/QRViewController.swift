3eb7d631c56f4b3915ba55981ab50bfa64578e9e
Ivan <romvano@gmail.com>
2017-12-20 14:16:01 +0300

    began qr, storyboard

--- BonusFeed/BonusFeed/QRViewController.swift
+++ BonusFeed/BonusFeed/QRViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class QRViewController: UIViewController {
+    
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var idLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let uid = UserModel.load()?.uid
+        guard uid != nil else {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let storyboard = UIStoryboard(name: "Login", bundle: nil)
+            let mainViewController = storyboard.instantiateViewController(withIdentifier: "LoginView")
+            appDelegate.window?.rootViewController = mainViewController
+            return
+        }
+        idLabel.text = uid
+        img = generateQRCode(from: uid!)
+    }
+    
+    func generateQRCode(from s: String) -> UIImageView? {
+        let data = s.data(using: String.Encoding.ascii)
+        
+        return nil
     }
 
     override func didReceiveMemoryWarning() {
